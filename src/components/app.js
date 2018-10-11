5f4b0488252c665e60494fc4fb23ec8fff978195
sahas-arora <sarora1@mail.sfsu.edu>
2018-10-11 15:37:19 -0700

    Cost of the event

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/searchBar.js';
 import EventName from '../containers/EventName.js';
+import EventCost from './eventCost.js';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
-      <EventName />
+        <EventName />
+      <EventCost />
       </div>
     );
   }
---
+++ src/components/eventCost.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class EventCost extends Component{
+
+  render() {
+    return (
+        <div>
+          Event Cost
+        </div>
+    );
+  }
+}
+
+export default EventCost;
